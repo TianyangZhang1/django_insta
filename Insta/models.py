0450e0ad5752fbaf2132129cbdc20c2c15e1fd06
Tianyang Zhang <353613258@qq.com>
2020-01-08 23:42:51 -0500

    add post model

--- Insta/models.py
+++ Insta/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.TextField()
+    images = ProcessedImageField(
+        upload_to='static/images',
+        format='JPEG',
+        options={'quality':100},
+        blank=True,
+        null= True,
+    )
