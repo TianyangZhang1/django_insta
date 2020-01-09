fb667b9c1d12b6c99c07c981c2478aa6cb6b7fa4
Tianyang Zhang <353613258@qq.com>
2020-01-08 23:53:50 -0500

    add post model

--- Insta/admin.py
+++ Insta/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from Insta.models import Post
+
+
+admin.site.register(Post)
--- Insta/models.py
+++ Insta/models.py
@@ -12,3 +12,6 @@ class Post(models.Model):
         blank=True,
         null= True,
     )
+
+    def __str__(self):
+        return self.title
